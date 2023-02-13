a9986cbdb161fd01986a9cca1f974ec164a4803c
Choudhary Rahul, SUB-SDC-NL-DOS-TDE <Rahul.Choudhary@swisscom.com>
2023-02-13 22:51:26 +0100

    Fourth

--- fourth_variables/terraform.tfvars
+++ fourth_variables/terraform.tfvars
@@ -1,3 +1,4 @@
+instance_type = "t2.micro"
 instance_type_1 = "t2.micro"
 instance_type_2 = { "micro" = "t2.micro", "small" = "t2.small" }
 instance_type_3 = [ "t2.micro", "t2.small" ]
--- fourth_variables/variables.tf
+++ fourth_variables/variables.tf
@@ -2,6 +2,15 @@ variable "vpn_ip" {
   default = "116.30.45.50/32"
 }
 
+variable "instance_type" {
+  type = string
+}
+
+variable "instance_names" {
+  type    = list(string)
+  default = ["dev-ec2-instance", "stg-ec2-instance", "prod-ec2-instance"]
+}
+
 variable "instance_type_1" {
   type = string
 }
