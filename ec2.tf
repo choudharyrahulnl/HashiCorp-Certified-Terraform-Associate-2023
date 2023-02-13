acc9b3c9be4a56bc728e5b75e42a78c756ac6f80
Choudhary Rahul, SUB-SDC-NL-DOS-TDE <Rahul.Choudhary@swisscom.com>
2023-02-13 13:12:26 +0100

    Creating EC2 Instance

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,22 @@
-variable "TF_ACCESS_KEY" { type = string }
-variable "TF_SECRET_KEY" { type = string }
-
 # configure the aws provider
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.54.0"
+    }
+  }
+}
+
 provider "aws" {
   region     = "eu-central-1"
   access_key = var.TF_ACCESS_KEY
   secret_key = var.TF_SECRET_KEY
 }
+
+# create variables for the access and secret key
+variable "TF_ACCESS_KEY" { type = string }
+variable "TF_SECRET_KEY" { type = string }
 
 # create a new ec2 instance
 resource "aws_instance" "ec2" {
