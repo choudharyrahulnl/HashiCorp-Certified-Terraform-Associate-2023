1ff621d30346d57af14cf207903160a34c78b60d
Choudhary Rahul, SUB-SDC-NL-DOS-TDE <Rahul.Choudhary@swisscom.com>
2023-02-14 23:39:03 +0100

    Sixteen

---
+++ seventeen_local_exec/local.tf
@@ -0,0 +1,12 @@
+# create a new ec2 instance
+resource "aws_instance" "ec2" {
+  ami           = "ami-06c39ed6b42908a36" # Check in AWS console for the Free Tier AMI
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-ec2"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.ec2.public_ip} > ip.txt"
+  }
+}
