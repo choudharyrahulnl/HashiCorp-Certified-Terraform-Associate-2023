00a63f3a4245341e3f0ee571d181434cfed9da5a
Choudhary Rahul, SUB-SDC-NL-DOS-TDE <Rahul.Choudhary@swisscom.com>
2023-02-14 20:50:09 +0100

    Fifteen

--- fifteen_foreach/foreach.tf
+++ fifteen_foreach/foreach.tf
@@ -17,3 +17,17 @@ resource "aws_iam_user" "user_foreach" {
   name = each.key
   path = "/system/"
 }
+
+# create ec2 instance using for_each: This will create two ec2 instances with different instance type
+resource "aws_instance" "web" {
+  ami           = "ami-06c39ed6b42908a36"
+  for_each = {
+    "key-1" = "t2.micro"
+    "key-2" = "t2.small"
+  }
+  instance_type = each.value
+  key_name = each.key
+  tags = {
+    Name = each.value
+  }
+}
