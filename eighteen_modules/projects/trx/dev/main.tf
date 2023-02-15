6b007b219eb9e6a3ed671ae1e4b69bfc3a510030
Choudhary Rahul, SUB-SDC-NL-DOS-TDE <Rahul.Choudhary@swisscom.com>
2023-02-15 20:32:17 +0100

    Eighteen

--- eighteen_modules/projects/trx/dev/main.tf
+++ eighteen_modules/projects/trx/dev/main.tf
@@ -13,3 +13,11 @@ resource "aws_instance" "web" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [module.sg.ec2_sg_id]
 }
+
+## terraform registry module
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  ami                    = "ami-06c39ed6b42908a36"
+  instance_type          = "t2.micro"
+  subnet_id = "subnet-0b8c888747c68e97f"
+}
